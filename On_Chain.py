60f6d80555d135931b8b61a0d2cf70d49fa8c3a2
Mattia Micheletta <mattiamicheletta@Mattias-Air.homenet.telecomitalia.it>
2023-05-16 14:55:10 +0200

    mmm

--- On_Chain.py
+++ On_Chain.py
@@ -154,7 +154,7 @@ with col2:
     # metric for current hashrate
     current_hash = round(hash_df.iloc[0]['Hash']/10**9, 2)
     delta = round((hash_df.iloc[0]['Hash'] - hash_df.iloc[1]['Hash'])/10**9, 2)
-    col2.metric(label="Hash rate attuale", value=f'{current_hash} TH/s', delta=f'{delta} TH/s')
+    col2.metric(label="Hash rate attuale", value=f'{current_hash} TH/s', delta=f'{delta} TH/s rispetto a 3 giorni fa')
     st.divider()
     # metric for current fees
     st.write("Commissioni (in sat/vB) per includere una transazione in ...")
